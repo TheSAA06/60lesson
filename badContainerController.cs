599689e520914f91b3efdb54521ad31eac042d67
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-02 09:56:24 +0900

    Add files via upload

---
+++ badContainerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class badContainerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        print("Не правильно");
+    }
+}
---
+++ goodContainerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goodContainerController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+}
